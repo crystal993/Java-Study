1437a540348aca5dcbdd012bfd15d155df9036ee
Playdata <Playdata@DESKTOP-DI937K9>
2021-05-29 22:36:07 +0900

    My first eclipse commit

---
+++ 01.hello/src/HelloWorld.java
@@ -0,0 +1,11 @@
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("피곤하다");
+		//이름 출력하기 
+		System.out.println("나의 이름은 김수정");
+        System.out.println("26");
+	}
+
+}
